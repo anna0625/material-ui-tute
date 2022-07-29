7c3ef1bc8c2ded26fcee8f1b1bb87d11718b0816
anna0625 <anna860625@gmail.com>
2022-07-29 20:14:08 +1000

    ok

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
-import Container from '@material-ui/core/Container'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Container from "@material-ui/core/Container";
 
 export default function Create() {
   return (
     <Container size="sm">
       <Typography
-        variant="h6" 
+        variant="h6"
         color="textSecondary"
         component="h2"
         gutterBottom
@@ -17,23 +17,30 @@ export default function Create() {
       </Typography>
 
       <Button
-        onClick={() => console.log('you clicked me')}
-        type="submit" 
-        color="secondary" 
-        variant="contained">
+        onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
         Submit
       </Button>
 
       <br />
 
-      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
-      <Button type="submit" color="default">Submit</Button>
-      
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+      <Button type="submit" color="default">
+        Submit
+      </Button>
+
+      <br />
+
       <ButtonGroup color="secondary" variant="contained" disableElevation>
         <Button>One</Button>
         <Button>Two</Button>
         <Button>Three</Button>
       </ButtonGroup>
     </Container>
-  )
+  );
 }
