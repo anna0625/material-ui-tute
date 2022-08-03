76c9b626c1833cfe55581228b1392d34b9bbeddb
anna0625 <anna860625@gmail.com>
2022-08-03 19:39:59 +1000

    add json server

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+// a component , so it can change to {ArrowRightIcon} from "@mui/icons-material"
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import { makeStyles } from "@material-ui/core/styles"; // a function
+
+// return a hook
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (details.length === "") {
+      setDetailsError(true);
+    }
+    if (title && details) {
+      console.log(title, details, category);
+      fetch("http://localhost:8000/notes", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, details, category }),
+      }).then(() => history.push("/"));
+    }
+  };
+
   return (
     <Container size="sm">
       <Typography
@@ -16,31 +63,58 @@ export default function Create() {
         Create a New Note
       </Typography>
 
-      <Button
-        onClick={() => console.log("you clicked me")}
-        type="submit"
-        color="secondary"
-        variant="contained"
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
 
-      <br />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
 
-      <Button type="submit" color="secondary" variant="outlined">
-        Submit
-      </Button>
-      <Button type="submit" color="default">
-        Submit
-      </Button>
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todes" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
 
-      <br />
-
-      <ButtonGroup color="secondary" variant="contained" disableElevation>
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          disableElevation
+          endIcon={<ArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
